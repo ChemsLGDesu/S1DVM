6282de9caf5ba388e55f5d6b21ed153535c7bdc7
ChemsLGDesu <jamesgonelozano@gmail.com>
2026-03-22 20:18:52 -0500

    Completado

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -1,48 +1,66 @@
 using UnityEngine;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class BaseEnemy : MonoBehaviour
 {
-    public GameObject Player;
-    public float range =3f;
-    public float speed;
+    public Transform Player;
+    public int range = 10;
+    public bool VerificarArea = false;
+    public float speed = 2f;
+    void Start()
+    {
+        GetComponent<SphereCollider>().radius = range;
+        GetComponent<Player>();
 
-    public bool enterZone;
-    public void OnTriggerEnter(Collider other)
+    }
+
+    void Update()
+    {
+        PerseguirPlayer();
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            enterZone = true;
-            print("Player Entered");
+            VerificarArea = true;
+            Player = other.gameObject.transform;
+            print("Player Detectado");
         }
     }
-    public void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            enterZone = false;
-            print("Player not Detect");
+            VerificarArea = false;
+            Player = null;
+            print("Player no Detectado");
         }
     }
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        DetectRange();
-    }
-    public void DetectRange()
-    {
-        if (enterZone)
+        if (collision.gameObject.CompareTag("Player"))
         {
-            if (range < 3f)
-            {
-                GameObject obj = Instantiate(Player, transform);
 
-                Vector3 origin = transform.position;
-                Vector3 FinalPosition = origin  * Random.Range(0, range);
-                obj.transform.position = FinalPosition;
-
-                Gizmos.color = Color.red;
-                Gizmos.DrawWireSphere(transform.position, 3f);
-            }
+            print("Enemigo Eliminado");
+            Destroy(gameObject);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+        Gizmos.color = Color.green;
+        if (VerificarArea)
+        {
+            Gizmos.DrawLine(transform.position, Player.transform.position);
+        }
+    }
+    public void PerseguirPlayer()
+    {
+        if (VerificarArea)
+        {
+            Vector3 dir = (Player.transform.position - transform.position).normalized;
+            transform.position += dir * speed * Time.deltaTime;
         }
     }
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    private Vector2 moveInput;
+    public InputSystem_Actions inputSystem;
+    public float speed = 5f;
+
+    private void OnEnable()
     {
-        
+        inputSystem = new InputSystem_Actions();
+        inputSystem.Enable();
+
+        inputSystem.Player.Move.started += Movement;
+        inputSystem.Player.Move.performed += Movement;
+        inputSystem.Player.Move.canceled += Movement;
     }
 
     void Update()
     {
-        
+        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+
+    private void Movement(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
     }
 }
